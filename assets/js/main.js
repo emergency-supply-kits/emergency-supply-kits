0ae0128a0e5db48dd625ceddb1f6d7c543d0b2ae
Roschelle <dsouza.roschelle123@gmail.com>
2021-04-19 14:21:16 -0500

    fix testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,28 @@
 */
 
 (function($) {
+	var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: 'https://formspree.io/f/myylzpvk',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+			$contactForm[0].reset();
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
 
 	var	$window = $(window),
 		$body = $('body'),
